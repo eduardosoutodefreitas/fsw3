69fd301b732394033ed8626a4b481df5ee1675b6
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-31 11:46:43 -0300

    style: add avatar fallback

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 const BookingItem = () => {
   return (
     <Card>
-      <CardContent className='flex px-5 justify-between'>
+      <CardContent className='flex px-5 py-0 justify-between'>
         <div className='flex flex-col gap-2 py-5'>
           <Badge className='bg-[#221c3d] text-primary hover:bg-[#221c3d]'>
             Confirmado
@@ -17,15 +17,9 @@ const BookingItem = () => {
           <h2 className='font-bold'>Corte de Cabelo</h2>
           <div className='flex items-center gap-2'>
             <Avatar className='h-6 w-6'>
-              <AvatarImage>
-                <Image
-                  src={
-                    "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png"
-                  }
-                  alt={"Barbershop image"}
-                />
-                <AvatarFallback></AvatarFallback>
-              </AvatarImage>
+              <AvatarImage src='https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png' />
+
+              <AvatarFallback>A</AvatarFallback>
             </Avatar>
             <h3 className='text-sm'>Vintage Barber</h3>
           </div>
